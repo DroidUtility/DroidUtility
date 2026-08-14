cd23460a0fc7ffeba4f9ff77c78bfb26d9df3bc0
Frostre_ <n9043395@gmail.com>
2026-08-14 23:42:40 +0200

    Fix import statements in FloatingBottomBar.kt

--- app/src/main/java/com/frostre1997/droidutility/navigation/FloatingBottomBar.kt
+++ app/src/main/java/com/frostre1997/droidutility/navigation/FloatingBottomBar.kt
@@ -2,7 +2,7 @@ package com.frostre1997.droidutility.navigation
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compute.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,8 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compute.ui.unit.dp
-import androidx.compute.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.frostre1997.droidutility.Screen
 
 @Composable
